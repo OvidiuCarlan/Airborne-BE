073bd5e62b22a5d122fafdecc3ed7bc9606eba3d
Ovidiu Cârlan <481968@student.fontys.nl>
2024-01-10 01:30:47 +0100

    Pipeline test 3

--- src/main/java/airborne/business/AddFriendUseCase.java
+++ src/main/java/airborne/business/AddFriendUseCase.java
@@ -3,6 +3,7 @@ package airborne.business;
 import airborne.business.dto.AddFriendRequest;
 import airborne.business.dto.AddFriendResponse;
 
+//for pipeline test
 public interface AddFriendUseCase {
     AddFriendResponse addFriend(AddFriendRequest request);
 }
